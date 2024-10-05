5dec3f6e15c81dbc1a8d4010ba10b069ec8a466e
Ece Temizkalb <ece.temizkalb@ug.bilkent.edu.tr>
2024-10-05 23:05:19 +0300

    Add project files.

---
+++ BLL/Models/StudentModel.cs
@@ -0,0 +1,16 @@
+﻿#nullable disable
+using BLL.DAL;
+
+namespace BLL.Models
+{
+    public class StudentModel
+    {
+        public Student Record { get; set; }
+        public int Id => Record.Id;
+        public string Name => Record.Name;
+        public string Surname => Record.Surname;
+        public DateTime BirthDate => (DateTime)Record.BirthDate;
+        public decimal Gpa => (decimal)Record.Gpa;
+
+    }
+}
